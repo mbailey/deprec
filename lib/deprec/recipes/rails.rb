8bc70dcc06863cfebbb6a14b248da365cd417c0f
Mike Bailey <mike@bailey.net.au>
2008-06-28 17:16:27 +1000

    * added 'git' to install_rails_stack * added task :install_rails_stack_no_config   - toying with the idea that a default config should be     installed during installation and that config_gen & config     should then called later. That was we can pre-install stacks     without worrying about the config at that time.

--- lib/deprec/recipes/rails.rb
+++ lib/deprec/recipes/rails.rb
@@ -236,9 +236,10 @@ Capistrano::Configuration.instance(:must_exist).load do
         top.deprec.nginx.install
         top.deprec.nginx.config
         
-        # Subversion
+        # Version control
         top.deprec.svn.install
-
+        top.deprec.git.install
+        
         # Ruby
         top.deprec.ruby.install      
         top.deprec.rubygems.install      
@@ -262,6 +263,20 @@ Capistrano::Configuration.instance(:must_exist).load do
         top.deprec.logrotate.install
         top.deprec.logrotate.config
         
+      end
+      
+      desc "install from anywhere"
+      task :install_rails_stack_no_config do
+        top.deprec.nginx.install
+        top.deprec.svn.install
+        top.deprec.git.install
+        top.deprec.ruby.install      
+        top.deprec.rubygems.install      
+        top.deprec.mongrel.install
+        top.deprec.monit.install
+        top.deprec.mysql.install
+        top.deprec.rails.install
+        top.deprec.logrotate.install        
       end
       
       desc "setup and configure servers"
