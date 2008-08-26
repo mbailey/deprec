9d26547f2184c57e1977c377c453c251c2ebd5d3
Mike Bailey <mike@bailey.net.au>
2008-08-27 00:12:18 +1000

    added dnsutils, provider of 'dig' (among other things)

--- lib/deprec/recipes/utils.rb
+++ lib/deprec/recipes/utils.rb
@@ -28,7 +28,7 @@ Capistrano::Configuration.instance(:must_exist).load do
         end
      
         task :net do
-          apps = %w(lynx nmap netcat mailx mutt telnet vim-full)
+          apps = %w(lynx nmap netcat mailx mutt telnet vim-full dnsutils)
           apt.install( {:base => apps}, :stable )
         end
         
