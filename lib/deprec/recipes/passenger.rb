af0a52f370c4464612f61c4128714d043aca982f
Mike Bailey <mike@bailey.net.au>
2009-01-27 23:56:12 +1100

    Install Passenger and Ruby Enterprise Edition

--- lib/deprec/recipes/passenger.rb
+++ lib/deprec/recipes/passenger.rb
@@ -3,10 +3,19 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deprec do 
     namespace :passenger do
       
-      set :passenger_install_dir, '/opt/passenger'
+      set :passenger_use_ree, true
+      
+      set(:passenger_install_dir) { 
+        if passenger_use_ree
+          "#{ree_install_dir}/lib/ruby/gems/1.8/gems/passenger-2.0.6"
+        else
+          '/opt/passenger'
+        end
+      }
+      
       set(:passenger_document_root) { "#{current_path}/public" }
       set :passenger_rails_allow_mod_rewrite, 'off'
-      set :passenger_vhost_dir, '/etc/apache2/sites-enabled'
+      set :passenger_vhost_dir, '/etc/apache2/sites-available'
       # Default settings for Passenger config files
       set :passenger_log_level, 0
       set :passenger_user_switching, 'on'
@@ -20,16 +29,63 @@ Capistrano::Configuration.instance(:must_exist).load do
       SRC_PACKAGES[:passenger] = {
         :url => "git://github.com/FooBarWidget/passenger.git",
         :download_method => :git,
-        :version => 'release-2.0.3', # Specify a tagged release to deploy
+        :version => 'release-2.0.6', # Specify a tagged release to deploy
         :configure => '',
         :make => '',
         :install => './bin/passenger-install-apache2-module'
       }
+
+      desc "Install passenger"
+      task :install, :roles => :app do
+        install_deps
+        deprec2.download_src(SRC_PACKAGES[:passenger], src_dir)
+
+        if passenger_use_ree
+          # Install the Passenger that came with Ruby Enterprise Edition
+          run "yes | #{sudo} env PATH=#{ree_install_dir}/bin:$PATH #{ree_install_dir}/bin/passenger-install-apache2-module"
+        else
+          # Non standard - passenger requires input
+          package_dir = File.join(src_dir, 'passenger.git')
+          dest_dir = passenger_install_dir + '-' + (SRC_PACKAGES[:passenger][:version] || 'trunk')
+          run "#{sudo} rsync -avz #{package_dir}/ #{dest_dir}"
+          run "cd #{dest_dir} && yes '' | #{sudo} ./bin/passenger-install-apache2-module"
+          run "#{sudo} unlink #{passenger_install_dir} 2>/dev/null; #{sudo} ln -sf #{dest_dir} #{passenger_install_dir}"
+        end
+        
+        initial_config_push
+        
+      end
+      
+      task :initial_config_push, :roles => :web do
+        # XXX Non-standard!
+        # We need to push out the .load and .conf files for Passenger
+        SYSTEM_CONFIG_FILES[:passenger].each do |file|
+          deprec2.render_template(:passenger, file.merge(:remote => true))
+        end
+      end
+
+      # Install dependencies for Passenger
+      task :install_deps, :roles => :app do
+        apt.install( {:base => %w(apache2-mpm-prefork apache2-prefork-dev rsync)}, :stable )
+        gem2.install 'fastthread'
+        gem2.install 'rack'
+        gem2.install 'rake'
+        # These are more Rails than Passenger - Mike
+        # gem2.install 'rails'
+        # gem2.install "mysql -- --with-mysql-config='/usr/bin/mysql_config'"
+        # gem2.install 'sqlite3-ruby'
+        # gem2.install 'postgres'
+      end
       
       SYSTEM_CONFIG_FILES[:passenger] = [
 
-        {:template => 'passenger.erb',
-          :path => '/etc/apache2/conf.d/passenger',
+        {:template => 'passenger.load.erb',
+          :path => '/etc/apache2/mods-available/passenger.load',
+          :mode => 0755,
+          :owner => 'root:root'},
+          
+        {:template => 'passenger.conf.erb',
+          :path => '/etc/apache2/mods-available/passenger.conf',
           :mode => 0755,
           :owner => 'root:root'}
 
@@ -38,33 +94,11 @@ Capistrano::Configuration.instance(:must_exist).load do
       PROJECT_CONFIG_FILES[:passenger] = [
 
         { :template => 'apache_vhost.erb',
-          :path => 'apache_vhost',
+          :path => "apache_vhost",
           :mode => 0755,
           :owner => 'root:root'}
 
       ]
-
-      desc "Install passenger"
-      task :install, :roles => :passenger do
-        install_deps
-        deprec2.download_src(SRC_PACKAGES[:passenger], src_dir)
-
-        # Non standard - passenger requires input
-        package_dir = File.join(src_dir, 'passenger.git')
-        dest_dir = passenger_install_dir + '-' + (SRC_PACKAGES[:passenger][:version] || 'trunk')
-        run "#{sudo} rsync -avz #{package_dir}/ #{dest_dir}"
-        run <<-EOF
-        cd #{dest_dir} &&
-        #{sudo} ruby -i -pe '$_ = $_.sub("STDIN.readline","# do nothing")' bin/passenger-install-apache2-module
-        EOF
-        run "cd #{dest_dir} && #{sudo} ./bin/passenger-install-apache2-module"
-        run "#{sudo} unlink #{passenger_install_dir} 2>/dev/null; #{sudo} ln -sf #{dest_dir} #{passenger_install_dir}"
-      end
-
-      # install dependencies for nginx
-      task :install_deps, :roles => :passenger do
-        apt.install( {:base => %w(apache2-mpm-prefork apache2-prefork-dev rsync)}, :stable )
-      end
        
       desc "Generate Passenger apache configs (system & project level)."
       task :config_gen do
@@ -87,24 +121,67 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
 
       desc "Push Passenger config files (system & project level) to server"
-      task :config, :roles => :passenger do
+      task :config, :roles => :app do
         config_system
         config_project  
       end
 
       desc "Push Passenger configs (system level) to server"
-      task :config_system, :roles => :passenger do
+      task :config_system, :roles => :app do
         deprec2.push_configs(:passenger, SYSTEM_CONFIG_FILES[:passenger])
+        activate_system
       end
 
       desc "Push Passenger configs (project level) to server"
-      task :config_project, :roles => :passenger do
+      task :config_project, :roles => :app do
         deprec2.push_configs(:passenger, PROJECT_CONFIG_FILES[:passenger])
         symlink_passenger_vhost
+        activate_project
       end
 
-      task :symlink_passenger_vhost, :roles => :passenger do
+      task :symlink_passenger_vhost, :roles => :app do
         sudo "ln -sf #{deploy_to}/passenger/apache_vhost #{passenger_vhost_dir}/#{application}"
+      end
+      
+      task :activate do
+        activate_system
+        activate_project
+      end
+      
+      task :activate_system do
+        sudo "a2enmod passenger"
+        top.deprec.web.reload
+      end
+      
+      task :activate_project do
+        sudo "a2ensite #{application}"
+        top.deprec.web.reload
+      end
+      
+      task :deactivate do
+        puts
+        puts "******************************************************************"
+        puts
+        puts "Danger!"
+        puts
+        puts "Do you want to deactivate just this project or all Passenger"
+        puts "projects on this server? Try a more granular command:"
+        puts
+        puts "cap deprec:passenger:deactivate_system  # disable Passenger"
+        puts "cap deprec:passenger:deactivate_project # disable only this project"
+        puts
+        puts "******************************************************************"
+        puts
+      end
+      
+      task :deactivate_system do
+        sudo "a2dismod passenger"
+        top.deprec.web.reload
+      end
+      
+      task :deactivate_project do
+        sudo "a2dissite #{application}"
+        top.deprec.web.reload
       end
       
       desc "Restart Application"
@@ -118,5 +195,37 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
       
     end
+    
+    namespace :ree do
+      
+      set :ree_version, 'ruby-enterprise-1.8.6-20090113'
+      set :ree_install_dir, "/opt/#{ree_version}"
+      set :ree_short_path, '/opt/ruby-enterprise'
+      
+      SRC_PACKAGES[:ree] = {
+        :md5sum => "e8d796a5bae0ec1029a88ba95c5d901d #{ree_version}.tar.gz",
+        :url => "http://rubyforge.org/frs/download.php/50087/#{ree_version}.tar.gz",
+        :configure => '',
+        :make => '',
+        :install => "./installer --auto /opt/#{ree_version}"
+      }
+ 
+      task :install do
+        install_deps
+        deprec2.download_src(SRC_PACKAGES[:ree], src_dir)
+        deprec2.install_from_src(SRC_PACKAGES[:ree], src_dir)
+        symlink_ree
+      end
+      
+      task :install_deps do
+        apt.install({:base => %w(libssl-dev libmysqlclient15-dev libreadline5-dev)}, :stable)
+      end
+      
+      task :symlink_ree do
+        sudo "ln -sf /opt/#{ree_version} #{ree_short_path}"
+      end
+      
+    end
+    
   end
 end
