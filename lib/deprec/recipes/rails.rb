22b778dd8b590f2789e981e02301d25cc6dc9035
Mike Bailey <mike@bailey.net.au>
2009-02-27 19:51:58 +1100

    Run the database rake tasks on the app server

--- lib/deprec/recipes/rails.rb
+++ lib/deprec/recipes/rails.rb
@@ -292,22 +292,22 @@ Capistrano::Configuration.instance(:must_exist).load do
     namespace :database do
       
       desc "Create database"
-      task :create, :roles => :db do
+      task :create, :roles => :app do
         run "cd #{deploy_to}/current && rake db:create RAILS_ENV=#{rails_env}"
       end
 
       desc "Run database migrations"
-      task :migrate, :roles => :db do
+      task :migrate, :roles => :app do
         run "cd #{deploy_to}/current && rake db:migrate RAILS_ENV=#{rails_env}"
       end
       
       desc "Run database migrations"
-      task :schema_load, :roles => :db do
+      task :schema_load, :roles => :app do
         run "cd #{deploy_to}/current && rake db:schema:load RAILS_ENV=#{rails_env}"
       end
 
       desc "Roll database back to previous migration"
-      task :rollback, :roles => :db do
+      task :rollback, :roles => :app do
         run "cd #{deploy_to}/current && rake db:rollback RAILS_ENV=#{rails_env}"
       end
 
