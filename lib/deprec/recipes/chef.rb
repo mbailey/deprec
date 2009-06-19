8dd52c830e598f4b6f59f50d2606c40353fa0652
Mike Bailey <mike@bailey.net.au>
2009-06-19 16:32:19 +1000

    chef updates

--- lib/deprec/recipes/chef.rb
+++ lib/deprec/recipes/chef.rb
@@ -3,21 +3,30 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deprec do 
     namespace :chef do
       
-      desc "Install Chef"
+      set(:server_fqdn) { Capistrano::CLI.ui.ask 'Enter Chef server hostname' }
+      set :recipes, 'chef::client'
+      
+      desc "Install Chef client"
       task :install do
-        install_deps
-        sudo "gem sources -a http://gems.opscode.com"
-        sudo "gem install ohai chef --no-rdoc --no-ri"
         config
-        sudo "sudo chef-solo -c /etc/chef/solo.rb -j /etc/chef/solo/chef.json -r http://s3.amazonaws.com/chef-solo/bootstrap-latest.tar.gz"
+        # install_deps
+        sudo 'chef-solo -c /etc/chef/solo.rb -j /etc/chef/chef.json -r http://s3.amazonaws.com/chef-solo/bootstrap-latest.tar.gz'
+      end
+      
+      task :install_server do
+        set :recipes, 'chef::recipes'
+        install
       end
       
       # Install dependencies for Chef
       task :install_deps, :roles => :chef do
         top.deprec.couchdb.install
         # top.deprec.ruby.install # XXX can we put this back in?
-        apt.install( {:base => %w(ruby ruby1.8-dev libopenssl-ruby1.8 rdoc ri irb)}, :stable )
+        # apt.install( {:base => %w(ruby ruby1.8-dev libopenssl-ruby1.8 rdoc ri irb)}, :stable )
+        top.deprec.ruby.install
         top.deprec.rubygems.install
+        sudo "gem sources -a http://gems.opscode.com"
+        sudo "gem install ohai chef --no-rdoc --no-ri"
       end
       
       SYSTEM_CONFIG_FILES[:chef] = [
@@ -27,8 +36,8 @@ Capistrano::Configuration.instance(:must_exist).load do
          :mode => 0644,
          :owner => 'root:root'},
          
-        {:template => "chef.json",
-         :path => '/etc/chef/solo/chef.json',
+        {:template => "chef.json.erb",
+         :path => '/etc/chef/chef.json',
          :mode => 0644,
          :owner => 'root:root'}
          
