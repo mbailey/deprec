06a01c0c9db5b09b34f66a8296aa93ecb1160f0f
Mike Bailey <mike@bailey.net.au>
2009-08-23 18:40:36 +1000

    Upgraded to nagios-3.2.0
    Added some useful default configs

--- lib/deprec/recipes/nagios.rb
+++ lib/deprec/recipes/nagios.rb
@@ -6,113 +6,128 @@ Capistrano::Configuration.instance(:must_exist).load do
       set :nagios_user, 'nagios'
       set :nagios_group, 'nagios'
       set(:nagios_host) { Capistrano::CLI.ui.ask "Enter hostname of nagios server" }
-      set :nagios_cmd_group, 'nagcmd' # Allow external commands to be submitted through the web interface
-      default :application, 'nagios' 
+      set(:nagios_ip) { Capistrano::CLI.ui.ask "Enter ip address of nagios server" }
+      set(:nagios_admin_pass) { Capistrano::CLI.ui.ask "Enter password for nagiosadmin user" }
+      set :nagios_cmd_group, 'nagcmd' # Submit external commands through the web interface
+      set :nagios_htpasswd_file, '/usr/local/nagios/etc/htpasswd.users'
+      # default :application, 'nagios' 
       
       SRC_PACKAGES[:nagios] = {
-        :url => "http://internap.dl.sourceforge.net/sourceforge/nagios/nagios-3.0.2.tar.gz",
-        :md5sum => "008d71aac08660bc007f7130ea82ab80  nagios-3.0.2.tar.gz", 
-        :configure => %w(
-          ./configure 
-          --with-command-group=nagcmd
-          ;
-          ).reject{|arg| arg.match '#'}.join(' '),
+        :url => "http://prdownloads.sourceforge.net/sourceforge/nagios/nagios-3.2.0.tar.gz",
+        :md5sum => "3566167cc60ddeaad34e7d2e26ed4a58  nagios-3.2.0.tar.gz", 
+        :configure => "./configure --with-command-group=nagcmd;",
         :make => 'make all;',
-        :install => 'make install install-init install-commandmode'
+        :install => 'make install install-init install-commandmode install-webconf;'
       }
       
+      desc "Install and configure Nagios server"
+      task :setup_server, :roles => :nagios do
+        install
+        top.deprec.nagios_plugins.install
+        top.deprec.nrpe.install
+        config_gen
+        config
+      end
+      
+      desc "Setup client"
+      task :setup_client do
+        top.deprec.nagios_plugins.install
+        top.deprec.nrpe.install
+        top.deprec.nrpe.config
+      end
+      
       desc "Install nagios"
-      task :install do
+      task :install, :roles => :nagios do
         install_deps
         create_nagios_user
-        deprec2.add_user_to_group(nagios_user, apache_user)
-        deprec2.mkdir('/usr/local/nagios/etc', :owner => "#{nagios_user}.#{nagios_group}", :via => :sudo)
-        deprec2.mkdir('/usr/local/nagios/objects', :owner => "#{nagios_user}.#{nagios_group}", :via => :sudo)
         deprec2.download_src(SRC_PACKAGES[:nagios], src_dir)
         deprec2.install_from_src(SRC_PACKAGES[:nagios], src_dir)
-        activate
       end
       
-      task :create_nagios_user do
+      # Install dependencies for nagios
+      task :install_deps, :roles => :nagios do
+        apt.install( {:base => %w(apache2 mailx postfix libapache2-mod-php5 libgd2-xpm-dev)}, :stable )
+      end
+      
+      task :create_nagios_user, :roles => :nagios do
         deprec2.groupadd(nagios_group)
         deprec2.useradd(nagios_user, :group => nagios_group, :homedir => false)
+        # deprec2.add_user_to_group(nagios_user, apache_user)
         deprec2.groupadd(nagios_cmd_group)
         deprec2.add_user_to_group(nagios_user, nagios_cmd_group)
-        # Add apache user to nagios group to permit commands via web interface
-        deprec2.add_user_to_group('daemon', nagios_cmd_group)
-      end
-         
-      # Install dependencies for nagios
-      task :install_deps do
-        apt.install( {:base => %w(mailx)}, :stable )
+        deprec2.add_user_to_group(apache_user, nagios_cmd_group)        
       end
       
+      desc "Grant a user access to the web interface"
+      task :htpass, :roles => :nagios do
+        target_user = Capistrano::CLI.ui.ask "Userid" do |q|
+          q.default = 'nagiosadmin'
+        end
+        system "htpasswd config/nagios/usr/local/nagios/etc/htpasswd.users #{target_user}"
+      end
+      
+      # desc "Set password for web based access"
+      # task :htpass do
+      #   target_user = Capistrano::CLI.ui.ask "Userid" do |q| 
+      #     q.default = 'nagiosadmin'
+      #   end
+      #   newpass = Capistrano::CLI.ui.ask "new password" do |q| 
+      #     q.echo = false 
+      #   end
+      #   sudo "htpasswd -b #{htpasswd_file} #{target_user} #{newpass}"
+      # end
+      
       SYSTEM_CONFIG_FILES[:nagios] = [
-        
-        {:template => 'nagios.cfg.erb',
-        :path => '/usr/local/nagios/etc/nagios.cfg',
-        :mode => 0664,
-        :owner => 'nagios:nagios'},
-
-        {:template => 'resource.cfg.erb',
-        :path => '/usr/local/nagios/etc/resource.cfg',
-        :mode => 0660,
-        :owner => 'nagios:nagios'},
         
         {:template => 'cgi.cfg.erb',
         :path => '/usr/local/nagios/etc/cgi.cfg',
         :mode => 0664,
         :owner => 'nagios:nagios'},
-
+        
         {:template => 'htpasswd.users',
         :path => '/usr/local/nagios/etc/htpasswd.users',
-        :mode => 0664,
-        :owner => 'nagios:nagios'},
-
-        {:template => 'templates.cfg.erb',
-        :path => '/usr/local/nagios/etc/objects/templates.cfg',
+        :mode => 0660,
+        :owner => 'nagios:www-data'},
+        
+        {:template => 'nagios.cfg.erb',
+        :path => '/usr/local/nagios/etc/nagios.cfg',
         :mode => 0664,
         :owner => 'nagios:nagios'},
         
-        {:template => 'commands.cfg.erb',
+        {:template => 'resource.cfg.erb',
+        :path => '/usr/local/nagios/etc/resource.cfg',
+        :mode => 0660,
+        :owner => 'nagios:nagios'},
+        
+        {:template => 'objects/commands.cfg.erb',
         :path => '/usr/local/nagios/etc/objects/commands.cfg',
         :mode => 0664,
         :owner => 'nagios:nagios'},
         
-        {:template => 'timeperiods.cfg.erb',
-        :path => '/usr/local/nagios/etc/objects/timeperiods.cfg',
-        :mode => 0664,
-        :owner => 'nagios:nagios'},
-        
-        {:template => 'localhost.cfg.erb',
-        :path => '/usr/local/nagios/etc/objects/localhost.cfg',
-        :mode => 0664,
-        :owner => 'nagios:nagios'},
-        
-        {:template => 'contacts.cfg.erb',
+        {:template => 'objects/contacts.cfg.erb',
         :path => '/usr/local/nagios/etc/objects/contacts.cfg',
         :mode => 0664,
         :owner => 'nagios:nagios'},
         
-        {:template => 'hosts.cfg.erb',
+        {:template => 'objects/hosts.cfg.erb',
         :path => '/usr/local/nagios/etc/objects/hosts.cfg',
         :mode => 0664,
         :owner => 'nagios:nagios'},
         
-        {:template => 'services.cfg.erb',
-        :path => '/usr/local/nagios/etc/objects/services.cfg',
-        :mode => 0664,
-        :owner => 'nagios:nagios'},
+        {:template => 'objects/localhost.cfg.erb',
+         :path => '/usr/local/nagios/etc/objects/localhost.cfg',
+         :mode => 0664,
+         :owner => 'nagios:nagios'},
+         
+        {:template => 'objects/services.cfg.erb',
+         :path => '/usr/local/nagios/etc/objects/services.cfg',
+         :mode => 0664,
+         :owner => 'nagios:nagios'},
         
-        {:template => 'localhost.cfg.erb',
-        :path => '/usr/local/nagios/etc/objects/localhost.cfg',
-        :mode => 0664,
-        :owner => 'nagios:nagios'},
-        
-        {:template => 'nagios_apache_vhost.conf.erb',
-         :path => "conf/nagios_apache_vhost.conf",
-         :mode => 0644,
-         :owner => 'root:root'}
+        {:template => 'objects/timeperiods.cfg.erb',
+         :path => '/usr/local/nagios/etc/objects/timeperiods.cfg',
+         :mode => 0664,
+         :owner => 'nagios:nagios'}
       
       ]
 
@@ -127,7 +142,6 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :config, :roles => :nagios do
         default :application, 'nagios'
         deprec2.push_configs(:nagios, SYSTEM_CONFIG_FILES[:nagios])
-        sudo "ln -sf #{deploy_to}/nagios/conf/nagios_apache_vhost.conf /usr/local/apache2/conf/apps"
         config_check
         restart
       end
@@ -137,18 +151,19 @@ Capistrano::Configuration.instance(:must_exist).load do
         send(run_method, "/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg")
       end
       
-      desc "Set Nagios to start on boot"
-      task :activate, :roles => :nagios do
-        send(run_method, "update-rc.d nagios defaults")
-        sudo "ln -sf #{deploy_to}/nagios/conf/nagios_apache_vhost.conf /usr/local/apache2/conf/apps"
-      end
-      
-      desc "Set Nagios to not start on boot"
-      task :deactivate, :roles => :nagios do
-        send(run_method, "update-rc.d -f nagios remove")
-        link = "#{apache_vhost_dir}/nagios_#{application}.conf"
-        sudo "test -h #{link} && sudo unlink #{link} || true"
-      end
+      # desc "Set Nagios to start on boot"
+      # task :activate, :roles => :nagios do
+      #   send(run_method, "update-rc.d nagios defaults")
+      #   sudo "a2ensite nagios"
+      #   top.deprec.apache.reload
+      # end
+      # 
+      # desc "Set Nagios to not start on boot"
+      # task :deactivate, :roles => :nagios do
+      #   send(run_method, "update-rc.d -f nagios remove")
+      #   sudo "a2dissite nagios"
+      #   top.deprec.apache.reload
+      # end
       
       # Control
 
@@ -179,25 +194,12 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :restore, :roles => :web do
         # not yet implemented
       end
-      
-      #
-      # Service specific tasks
-      #
-      
-      # XXX quick and dirty - clean up later
-      desc "Grant a user access to the web interface"
-      task :htpass, :roles => :nagios do
-        target_user = Capistrano::CLI.ui.ask "Userid" do |q|
-          q.default = 'nagiosadmin'
-        end
-        system "htpasswd config/nagios/usr/local/nagios/etc/htpasswd.users #{target_user}"
-      end
     
     end
     
     SRC_PACKAGES[:nagios_plugins] = {
-      :url => "http://downloads.sourceforge.net/nagiosplug/nagios-plugins-1.4.12.tar.gz",
-      :md5sum => "af68d00bbe2c39de02803d23e5eecca3  nagios-plugins-1.4.12.tar.gz", 
+      :url => "http://prdownloads.sourceforge.net/sourceforge/nagiosplug/nagios-plugins-1.4.13.tar.gz",
+      :md5sum => "be6cc7699fff3ee29d1fd4d562377386  nagios-plugins-1.4.13.tar.gz", 
       :configure => "./configure --with-nagios-user=#{nagios_user} --with-nagios-group=#{nagios_group};",
     }   
           
@@ -206,7 +208,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       desc "Install nagios plugins"
       task :install do
         install_deps
-        top.deprec.nagios.create_nagios_user
+        create_nagios_user
         deprec2.download_src(SRC_PACKAGES[:nagios_plugins], src_dir)
         deprec2.install_from_src(SRC_PACKAGES[:nagios_plugins], src_dir)        
       end
@@ -214,6 +216,15 @@ Capistrano::Configuration.instance(:must_exist).load do
       # Install dependencies for nagios plugins
       task :install_deps do
         apt.install( {:base => %w(libmysqlclient15-dev)}, :stable )
+      end
+      
+      task :create_nagios_user do
+        deprec2.groupadd(nagios_group)
+        deprec2.useradd(nagios_user, :group => nagios_group, :homedir => false)
+        # deprec2.add_user_to_group(nagios_user, apache_user)
+        deprec2.groupadd(nagios_cmd_group)
+        deprec2.add_user_to_group(nagios_user, nagios_cmd_group)
+        deprec2.add_user_to_group(apache_user, nagios_cmd_group)        
       end
       
       
@@ -236,7 +247,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       desc 'Install NRPE'
       task :install do
         install_deps
-        top.deprec.nagios.create_nagios_user
+        create_nagios_user
         deprec2.download_src(SRC_PACKAGES[:nrpe], src_dir)
         deprec2.install_from_src(SRC_PACKAGES[:nrpe], src_dir)
         # XXX this should only be run on the nrpe clients
@@ -247,6 +258,15 @@ Capistrano::Configuration.instance(:must_exist).load do
       
       task :install_deps do
         apt.install( {:base => %w(xinetd libssl-dev openssl)}, :stable )
+      end
+      
+      task :create_nagios_user do
+        deprec2.groupadd(nagios_group)
+        deprec2.useradd(nagios_user, :group => nagios_group, :homedir => false)
+        # deprec2.add_user_to_group(nagios_user, apache_user)
+        deprec2.groupadd(nagios_cmd_group)
+        deprec2.add_user_to_group(nagios_user, nagios_cmd_group)
+        deprec2.add_user_to_group(apache_user, nagios_cmd_group)        
       end
       
       SYSTEM_CONFIG_FILES[:nrpe] = [
@@ -289,10 +309,12 @@ Capistrano::Configuration.instance(:must_exist).load do
         sudo "/etc/init.d/xinetd start"  
       end
       
+      desc "Test whether NRPE is listening on client"
       task :test_local do
         run "/usr/local/nagios/libexec/check_nrpe -H localhost"
       end
       
+      desc "Test whether nagios server can query client via NRPE"
       task :test_remote, :roles => :nagios do
         target_host = Capistrano::CLI.ui.ask "target hostname"
         run "/usr/local/nagios/libexec/check_nrpe -H #{target_host}"
@@ -300,6 +322,9 @@ Capistrano::Configuration.instance(:must_exist).load do
   
     end
       
+    # PNP4nagios
+    # http://downloads.sourceforge.net/sourceforge/pnp4nagios/pnp-0.4.14.tar.gz?use_mirror=internode
+    
     
   end
 end
