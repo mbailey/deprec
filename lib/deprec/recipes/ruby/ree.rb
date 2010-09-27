79ebadd2c53f648f47deb1fb5c6c05680c0c35f8
Mike Bailey <mike@bailey.net.au>
2010-09-27 15:45:03 +1000

    install hardy-x86 deb package of ree

--- lib/deprec/recipes/ruby/ree.rb
+++ lib/deprec/recipes/ruby/ree.rb
@@ -6,7 +6,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       set :ree_install_dir, "/usr/local"
       
       SRC_PACKAGES[:ree] = {
-        :md5sum => "0eaff4bcd0bc9fa310c593be7ae33937  ruby-enterprise_1.8.7-2010.02_amd64_ubuntu8.04.deb",
+        :md5sum => "62e7ee838532744d5a155a16571f782e  ruby-enterprise_1.8.7-2010.02_i386_ubuntu8.04.deb",
         :url => "http://rubyforge.org/frs/download.php/71097/ruby-enterprise_1.8.7-2010.02_amd64_ubuntu8.04.deb",
         :download_method => :deb
       }
@@ -19,7 +19,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
       
       task :install_deps do
-        # not required with new dev package?
+        # not required with new deb package?
         # apt.install({:base => %w(libssl-dev libmysqlclient15-dev libreadline5-dev)}, :stable)
       end
       
