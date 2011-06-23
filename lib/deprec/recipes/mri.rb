29aed678cec27ac9c389a074949235ed47a2ba33
Mike Bailey <mike@bailey.net.au>
2011-06-24 01:58:06 +1000

    ruby-1.9.2-p180

--- lib/deprec/recipes/mri.rb
+++ lib/deprec/recipes/mri.rb
@@ -4,21 +4,21 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deprec do
     namespace :mri do
             
-      SRC_PACKAGES[:mri_1_8_7] = {
+      SRC_PACKAGES['ruby-1.8.7-p330'] = {
         :md5sum => "50a49edb787211598d08e756e733e42e  ruby-1.8.7-p330.tar.gz",
         :url => "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7-p330.tar.gz",
         :deps => %w(zlib1g-dev libssl-dev libncurses5-dev libreadline5-dev),
         :configure => "./configure --with-readline-dir=/usr/local;"
       }
 
-      SRC_PACKAGES[:mri_1_9_2] = {
-        :md5sum => "6e17b200b907244478582b7d06cd512e  ruby-1.9.2-p136.tar.gz", 
-        :url => "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p136.tar.gz",
+      SRC_PACKAGES['ruby-1.9.2-p180'] = {
+        :md5sum => "0d6953820c9918820dd916e79f4bfde8  ruby-1.9.2-p180.tar.gz", 
+        :url => "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz",
         :deps => %w(zlib1g-dev libssl-dev libncurses5-dev libreadline5-dev),
-        :configure => "./configure --with-readline-dir=/usr/local;"
+        :configure => "./configure"
       }
 
-      src_package_options = SRC_PACKAGES.keys.select{|k| k.to_s.match /^mri_/ }
+      src_package_options = SRC_PACKAGES.keys.select{|k| k.match /^ruby-\d\.\d\.\d/ }
       set(:mri_src_package) { Capistrano::CLI.ui.choose *src_package_options }
 
       desc "Install Ruby"
