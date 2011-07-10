fe04e2ee5b7de2e7ae776cc86562e230ac60b8ae
Mike Bailey <mike@bailey.net.au>
2011-07-10 21:09:54 +1000

    Tidied up Apache recipes

--- lib/deprec/recipes/apache.rb
+++ lib/deprec/recipes/apache.rb
@@ -13,17 +13,12 @@ Capistrano::Configuration.instance(:must_exist).load do
       set :apache_modules_enabled, 
         %w(rewrite ssl proxy_balancer proxy_http deflate expires headers)
 
-      desc "Install apache"
+      desc "Install Apache"
       task :install do
-        install_deps
-        config
-      end
-      
-      # install dependencies for apache
-      task :install_deps do
         apt.install( 
           {:base => %w(apache2-mpm-prefork apache2-prefork-dev rsync ssl-cert)},
           :stable )
+        config
       end
       
       SYSTEM_CONFIG_FILES[:apache] = [
@@ -59,43 +54,30 @@ Capistrano::Configuration.instance(:must_exist).load do
         # Not required
       ]
 
-      desc "Generate configuration file(s) for apache from template(s)"
+      desc "Generate Apache config files"
       task :config_gen do
         config_gen_system
       end
 
+      # Generate system level apache configs
       task :config_gen_system do
         SYSTEM_CONFIG_FILES[:apache].each do |file|
           deprec2.render_template(:apache, file)
         end
       end
 
+      # Not used here
       task :config_gen_project do
-        PROJECT_CONFIG_FILES[:apache].each do |file|
-          deprec2.render_template(:apache, file)
-        end
-        # XXX Need to flesh out generation of custom certs
-        # In the meantime we'll just use the snakeoil cert
-        #
-        # top.deprec.ssl.config_gen if apache_ssl_enabled
-        top.deprec.ssl.config_gen if apache_ssl_enabled
       end
       
-      desc "Push apache config files to server"
+      desc "Push Apache config files to server"
       task :config, :roles => :web do
         config_system
         enable_modules
         reload
       end
       
-      desc "Generate initial configs and copy direct to server."
-      task :initial_config, :roles => :web do
-        SYSTEM_CONFIG_FILES[:apache].each do |file|
-          deprec2.render_template(:apache, file.merge(:remote => true))
-        end
-      end
-      
-      desc "Push apache config files to server"
+      desc "Push Apache config files to server"
       task :config_system, :roles => :web do
         deprec2.push_configs(:apache, SYSTEM_CONFIG_FILES[:apache])
         run "#{sudo} touch /var/www/check.txt" # Used to test webserver up
@@ -112,46 +94,43 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :enable_modules, :roles => :web do
         apache_modules_enabled.each { |mod| sudo "a2enmod #{mod}" }
       end
+
+      #
+      # Control
+      #
       
       desc "Start Apache"
       task :start, :roles => :web do
-        send(run_method, "/etc/init.d/apache2 start")
+        run "#{sudo} /etc/init.d/apache2 start"
       end
 
       desc "Stop Apache"
       task :stop, :roles => :web do
-        send(run_method, "/etc/init.d/apache2 stop")
+        run "#{sudo} /etc/init.d/apache2 stop"
       end
 
       desc "Restart Apache"
       task :restart, :roles => :web do
-        send(run_method, "/etc/init.d/apache2 restart")
+        run "#{sudo} /etc/init.d/apache2 restart"
       end
 
       desc "Reload Apache"
       task :reload, :roles => :web do
-        send(run_method, "/etc/init.d/apache2 force-reload")
+        run "#{sudo} /etc/init.d/apache2 force-reload"
       end
 
-      desc "Set apache to start on boot"
+      desc "Set Apache to start on boot"
       task :activate, :roles => :web do
-        send(run_method, "update-rc.d apache2 defaults")
+        run "#{sudo} update-rc.d apache2 defaults"
       end
       
-      desc "Set apache to not start on boot"
+      desc "Set Apache to not start on boot"
       task :deactivate, :roles => :web do
-        send(run_method, "update-rc.d -f apache2 remove")
+        run "#{sudo} update-rc.d -f apache2 remove"
       end
       
-      task :backup, :roles => :web do
-        # not yet implemented
-      end
-      
-      task :restore, :roles => :web do
-        # not yet implemented
-      end
-
-      # Start apache vhost extras
+      # Apache vhost extras
+      #
       # These are only used for generating vhost files with: 
       #
       #   cap deprec:apache:vhost
