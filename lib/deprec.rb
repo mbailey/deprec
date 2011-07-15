2e824b4c57fbe9994c21c08d2abd2d403f422a20
Mike Bailey <mike@bailey.net.au>
2011-07-15 21:37:48 +1000

    Moved recipes. Require deprec-core

--- lib/deprec.rb
+++ lib/deprec.rb
@@ -1,6 +1,5 @@
+require 'deprec-core'
 if defined?(Capistrano)
-  require "#{File.dirname(__FILE__)}/deprec/capistrano_extensions"
-  require "#{File.dirname(__FILE__)}/vmbuilder_plugins/all"
   require "#{File.dirname(__FILE__)}/deprec/recipes"
 elsif defined?(Rake)
   require "#{File.dirname(__FILE__)}/deprec/rake"
