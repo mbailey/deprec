7dfc91dd1d3ac5fcc09be7e02ef764c76e46ae8a
Mike Bailey <mike@bailey.net.au>
2011-07-15 20:52:17 +1000

    Display deprecated notice for deprec_minus_rails

--- lib/deprec_minus_rails.rb
+++ lib/deprec_minus_rails.rb
@@ -1,12 +1,22 @@
-# DEPRECATION WARNING!
-#
-# deprec3 does not load deprec/recipes/rails.rb
-#
-# rails.rb originally provided callbacks for Capistrano's 
-# deploy tasks. This was a cause of surprise for some 
-# people so 'deprec_minus_rails' became a default for a 
-# while.
-#
-# deprec_minus_rails is now..erm...deprecated
-#
+puts <<-EOF
+    DEPRECATION WARNING!
+
+    rails.rb originally provided callbacks for Capistrano's 
+    deploy tasks. This was a cause of surprise for some 
+    people so 'deprec_minus_rails' became a safe default 
+    way for people to load deprec.
+
+    deprec3 does not load deprec/recipes/rails.rb
+    so deprec_minus_rails is now..erm...deprecated
+
+    You should replace:
+
+      require 'deprec_minus_rails'
+
+    with:
+
+      require 'deprec'
+
+EOF
+
 require "#{File.dirname(__FILE__)}/deprec"
