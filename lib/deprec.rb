80ebb949cd800cc2ca8ac20c1ac292c2178d0f2e
Mike Bailey <mike@bailey.net.au>
2011-07-15 21:46:06 +1000

    bugfix - recipes dir is now cap/

--- lib/deprec.rb
+++ lib/deprec.rb
@@ -1,6 +1,6 @@
 require 'deprec-core'
 if defined?(Capistrano)
-  require "#{File.dirname(__FILE__)}/deprec/recipes"
+  require "#{File.dirname(__FILE__)}/deprec/cap"
 elsif defined?(Rake)
   require "#{File.dirname(__FILE__)}/deprec/rake"
 end
