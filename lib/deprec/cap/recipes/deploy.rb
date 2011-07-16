a9861cb4da9ebd04a8e7845f03cd556419dcd79d
Mike Bailey <mike@bailey.net.au>
2011-07-16 23:09:03 +1000

    Extra tasks around Capistrano deploy recipes

---
+++ lib/deprec/cap/recipes/deploy.rb
@@ -0,0 +1,18 @@
+namespace :deprec do
+  namespace :deploy do
+
+    task :chown, :roles => :app do
+      run "#{sudo} chown -R #{user} #{deploy_to}"
+    end
+
+    task :symlink_database_yml, :roles => :app do
+      run "ln -nfs #{shared_path}/system/database.yml #{release_path}/config/database.yml"
+    end
+
+    task :symlink_backups, :roles => :app do
+      run "test -d #{shared_path}/system/backups || mkdir #{shared_path}/system/backups"
+      run "ln -nfs #{shared_path}/system/backups #{latest_release}/db/backups"
+    end
+    
+  end
+end
