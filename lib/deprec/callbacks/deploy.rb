b94d8d9763d641612348bbcfcb85d9c94c818ead
Mike Bailey <mike@bailey.net.au>
2011-07-17 03:14:54 +1000

    bugfix: include path in calback

--- lib/deprec/callbacks/deploy.rb
+++ lib/deprec/callbacks/deploy.rb
@@ -1,6 +1,6 @@
 # Copyright 2006-2008 by Mike Bailey. All rights reserved.
 Capistrano::Configuration.instance(:must_exist).load do
-  after 'deploy:setup', 'deprec:deploy:chown'
+  after 'deploy:setup',       'deprec:deploy:chown'
   after 'deploy:update_code', 'deprec:deploy:symlink_database_yml'
-  after 'deploy:update_code', 'symlink_backups'
+  after 'deploy:update_code', 'deprec:deploy:symlink_backups'
 end
