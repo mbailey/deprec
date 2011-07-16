1ff6e72112d43c34f504ec1aef8a279abb059533
Mike Bailey <mike@bailey.net.au>
2011-07-17 01:24:32 +1000

    Added deploy callbacks

--- lib/deprec/callbacks/deploy.rb
+++ lib/deprec/callbacks/deploy.rb
@@ -1,3 +1,6 @@
-after 'deploy:setup', 'deprec:deploy:chown'
-after 'deploy:update_code', 'deprec:deploy:symlink_database_yml'
-after 'deploy:update_code', 'symlink_backups'
+# Copyright 2006-2008 by Mike Bailey. All rights reserved.
+Capistrano::Configuration.instance(:must_exist).load do
+  after 'deploy:setup', 'deprec:deploy:chown'
+  after 'deploy:update_code', 'deprec:deploy:symlink_database_yml'
+  after 'deploy:update_code', 'symlink_backups'
+end
