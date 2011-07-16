485243c7abe84c0a6fba61e959d07d2ee94e5e43
Mike Bailey <mike@bailey.net.au>
2011-07-17 03:52:31 +1000

    bugfix: typo

--- lib/deprec.rb
+++ lib/deprec.rb
@@ -2,5 +2,5 @@ require 'deprec-core'
 if defined?(Capistrano)
   Dir.glob("#{File.dirname(__FILE__)}/deprec/recipes/*.rb").each { |t| require t }
 elsif defined?(Rake)
-  Dir.glob("#{File.dirname(__FILE__)}deprec/recipes/*.rake").each { |t| import t }
+  Dir.glob("#{File.dirname(__FILE__)}/deprec/recipes/*.rake").each { |t| import t }
 end
