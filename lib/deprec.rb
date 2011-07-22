129ffbd327d006fda9a1da8ab39e8161c0c8f2c9
Mike Bailey <mike@bailey.net.au>
2011-07-22 22:03:55 +1000

    Added TEMPLATE_LOAD_PATH
    deprec-core needs to know where to look for templates

--- lib/deprec.rb
+++ lib/deprec.rb
@@ -1,4 +1,9 @@
+TEMPLATE_LOAD_PATH = []
+TEMPLATE_LOAD_PATH << File.expand_path(
+  File.join(File.dirname(__FILE__),'deprec','templates')
+)
 require 'deprec-core'
+
 if defined?(Capistrano)
   Dir.glob("#{File.dirname(__FILE__)}/deprec/recipes/*.rb").each { |t| require t }
 elsif defined?(Rake)
